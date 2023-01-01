a139b078c9f0a651682a02b0051778176813539a
tv-12-melnyk-ihor <114071900+tv-12-melnyk-ihor@users.noreply.github.com>
2023-01-01 21:44:13 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,48 +2,33 @@ from fractions import Fraction
 
 class Rational:
 
-    def __init__(self, firstnum, secondnum):
-        if ((isinstance(firstnum, int) == False and isinstance(firstnum, float) == False) or
-                (isinstance(secondnum, int) == False and isinstance(secondnum, float) == False)):
-            raise Exception("Both arguments have to be rational numbers")
-        self.first = firstnum
-        self.second = secondnum
+    def __init__(self, num):
+        self.number = num
 
-    def add(self):
-        return Fraction(self.first + self.second).limit_denominator()
+    def __add__(self, obj):
+        return Fraction(self.number + obj.number).limit_denominator()
 
-    def subtract(self):
-        return Fraction(self.first - self.second).limit_denominator()
+    def __sub__(self, obj):
+        return Fraction(self.number - obj.number).limit_denominator()
 
-    def multiply(self):
-        return Fraction(self.first * self.second).limit_denominator()
+    def __mul__(self, obj):
+        return Fraction(self.number * obj.number).limit_denominator()
 
-    def divide(self):
-        return Fraction(self.first / self.second).limit_denominator()
+    def __truediv__(self, obj):
+        return Fraction(self.number / obj.number).limit_denominator()
 
-    def compare(self):
-        if self.first > self.second:
-            return f'{self.first} > {self.second}'
-        elif self.first < self.second:
-            return f'{self.first} < {self.second}'
-        else:
-            return f'{self.first} = {self.second}'
+    def __gt__(self, obj):
+        if self.number > obj.number:
+            return True
+        return False
 
 
 if __name__ == '__main__':
-    obj1 = Rational(3.4, 0.3)
-    print(obj1.add())
-    print(obj1.subtract())
-    print(obj1.multiply())
-    print(obj1.divide())
-    print(obj1.compare(), '\n')
-    obj2 = Rational(4.4, 13.2)
-    print(obj2.add())
-    print(obj2.subtract())
-    print(obj2.multiply())
-    print(obj2.divide())
-    print(obj2.compare(), '\n')
-
-
-
-
+    obj1 = Rational(5.4)
+    obj2 = Rational(0.9)
+    print(obj1 + obj2)
+    print(obj1 - obj2)
+    print(obj1 * obj2)
+    print(obj1 / obj2)
+    print(obj1 > obj2)
+    print(obj1 < obj2)
